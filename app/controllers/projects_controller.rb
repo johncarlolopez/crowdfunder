76c36a74d427fbebb215c6b1f959790bc7dda1a3
ANDREW SEGAL <segal.a@hotmail.com>
2018-01-29 16:43:41 -0500

    add search by category functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
     @projects = @projects.order(:end_date)
+    @categories = Category.all
   end
 
   def show
