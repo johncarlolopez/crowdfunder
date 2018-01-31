5ac800036bc7b15b83fd91387d18189f43446477
jeffrothwell <rothwelljeff@gmail.com>
2018-01-30 21:42:01 -0500

    create custom validations for max_claims

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,7 +2,20 @@ class Reward < ActiveRecord::Base
   belongs_to :project
 
   validates :description, :dollar_amount, presence: true
-  validates :dollar_amount, :max_claims, numericality: { greater_than: 0 }
-  validates :max_claims, numericality: { only_integer: true }
+  validates :dollar_amount, numericality: { greater_than: 0 }
+  validate :when_max_claims_supplied_must_be_integer,
+          :when_max_claims_supplied_must_be_greater_than_zero
+
+  def when_max_claims_supplied_must_be_integer
+    if max_claims.present? && !max_claims.integer?
+      errors.add(:max_claims, ", if supplied, must be an integer")
+    end
+  end
+
+  def when_max_claims_supplied_must_be_greater_than_zero
+    if max_claims.present? && max_claims <= 0
+      errors.add(:max_claims, ", if supplied, must be a positive number")
+    end
+  end
 
 end
