16f7a5b6727da5fd281e27abc9c78408fe2f769e
jeffrothwell <rothwelljeff@gmail.com>
2018-01-31 12:19:04 -0500

    correct dollar amount validation, zero is not allowed

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,7 +3,7 @@ class Pledge < ApplicationRecord
   belongs_to :project
   belongs_to :backed_project, class_name: "Project", foreign_key: :project_id
 
-  validates :dollar_amount, presence: true, numericality:{greater_than_or_equal_to: 0}
+  validates :dollar_amount, presence: true, numericality:{greater_than: 0}
   validates :user, presence: true
   validate :check_if_owner
 
