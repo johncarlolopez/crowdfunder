ec9590328194c627f4a9ecb0fe4642b447eb680f
jeffrothwell <rothwelljeff@gmail.com>
2018-01-31 12:20:17 -0500

    refactor tests using pledge factory

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -3,103 +3,35 @@ require_relative '../test_helper'
 class PledgeTest < ActiveSupport::TestCase
 
   test 'A pledge can be created' do
-    pledge = Pledge.create(
-      dollar_amount: 99.00,
-      project: new_project,
-      user: new_user
-    )
+    pledge = build(:pledge)
     pledge.save
     assert pledge.valid?
     assert pledge.persisted?
   end
 
   test 'owner cannot back own project' do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
-    pledge = Pledge.new(dollar_amount: 3.00, project: project)
-    pledge.user = owner
+    owner = create(:user)
+    pledge = build(:pledge, user: owner)
+    pledge.project.user = owner
     pledge.save
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
 
   test 'Pledge must have dollar amount' do
-     project = new_project
-     owner = new_user
-     owner.save
-     project.user_id = owner.id
-     project.save
-     user = new_user2
-     pledge = new_pledge
-     pledge.dollar_amount = nil
+    pledge = build(:pledge, dollar_amount: nil)
+    pledge.save
+    assert pledge.invalid?, 'Pledge should be invalid if there is no dollar amount'
+  end
+
+  test 'Pledge dollar amount must be above zero' do
+     pledge = build(:pledge, dollar_amount: 0)
      pledge.save
-     assert pledge.invalid?, 'Pledge should be invalid if there is no dollar amount'
+     assert pledge.invalid?, 'Pledge should be invalid if dollar amount is <= 0'
   end
 
-  test 'Pledge must be above zero' do
-     project = new_project
-     owner = new_user
-     owner.save
-     project.user_id = owner.id
-     project.save
-     user = new_user2
-     pledge = new_pledge
-     pledge.dollar_amount = -1
-     pledge.save
-     assert pledge.invalid?, 'Pledge should be invalid if dollar amount is under $0'
-  end
-
-  test 'Pledge creates if dollar amount is zero or above ' do
-     project = new_project
-     owner = new_user
-     owner.save
-     project.user_id = owner.id
-     project.save
-     user = new_user2
-     pledge = new_pledge
-     pledge.dollar_amount = 0
-     pledge.save
-     assert pledge.valid?, 'Pledge should be valid if dollar amount is over or equal to $0'
-
-  end
-
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
-  end
-
-  def new_user
-    User.new(
-      first_name:            'Sally',
-      last_name:             'Lowenthal',
-      email:                 'sally@example.com',
-      password:              'passpass',
-      password_confirmation: 'passpass'
-    )
-  end
-
-  def new_user2
-    User.new(
-      first_name:            'Bob',
-      last_name:             'Doel',
-      email:                 'bob@example.com',
-      password:              'passpass2',
-      password_confirmation: 'passpass2'
-    )
-  end
-
-  def new_pledge
-    Pledge.new(
-      dollar_amount: 99.00,
-      project: new_project,
-      user: new_user2
-    )
+  test 'Pledge must have a user assigned' do
+    pledge = build(:pledge, user: nil)
+    pledge.save
+    assert pledge.invalid?, 'Pledge should be invalid if no user assigned'
   end
 end
